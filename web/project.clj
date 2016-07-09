eca251dc5d6435d6e6e26524b097c5359fe9c181
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-07-08 23:09:54 -0300

    1.8.0

--- web/project.clj
+++ web/project.clj
@@ -1,5 +1,5 @@
 (defproject web "1.0.0-SNAPSHOT"
   :description "basic web project"
-  :dependencies [[org.clojure/clojure "1.6.0"] [org.clojure/data.json "0.1.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"] [org.clojure/data.json "0.1.1"]]
   :profiles {:uberjar {:aot :all}}
   :main web.core)
