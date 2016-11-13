b2b48e5ca4fac8dc1d7cbf4abed0a0b5f25caaa8
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-11-13 21:49:14 -0200

    aws-lambda

---
+++ lambda-demo/src/example/lambda.clj
@@ -0,0 +1,17 @@
+
+(ns example.lambda
+  (:require [uswitch.lambada.core :refer [deflambdafn]]
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]))
+
+(defn handle-event
+  [event]
+  (println "Got the following event: " (pr-str event))
+  {:status "ok"})
+
+(deflambdafn example.lambda.MyLambdaFn
+  [in out ctx]
+  (let [event (json/read (io/reader in))
+        res (handle-event event)]
+    (with-open [w (io/writer out)]
+      (json/write res w))))
