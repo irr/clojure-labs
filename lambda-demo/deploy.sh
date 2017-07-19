3416d5d93136754127cc23c021c88f4326676aa3
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2017-07-18 22:43:51 -0300

    512

--- lambda-demo/deploy.sh
+++ lambda-demo/deploy.sh
@@ -4,7 +4,7 @@ aws lambda create-function \
   --function-name clj-demo \
   --handler example.lambda.MyLambdaFn \
   --runtime java8 \
-  --memory 128 \
-  --timeout 10 \
+  --memory 512 \
+  --timeout 60 \
   --role arn:aws:iam::940851196820:role/lambda_pushManager \
   --zip-file fileb://./target/my-lambda-project.jar
