720c2f6082832b09adfdba83fbd88e47396587d0
Ivan Ribeiro Rocha <ivan.ribeiro@gmail.com>
2023-09-08 21:57:07 +0100

    reverted to original

--- web/src/web/core.clj
+++ web/src/web/core.clj
@@ -1,8 +1,6 @@
-(ns web.core #_{:clj-kondo/ignore [:use]}
-             (:use clojure.data.json) (:gen-class))
+(ns web.core (:use clojure.data.json) (:gen-class))
+
 (defn -main [& args]
   (println (json-str (list 1 2 3)) args))
 
 (-main)
-
-
